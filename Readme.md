d3c6f5e0391d0406193b1f4dbe3c672c2d011557
motiveko <rhehdrla@naver.com>
2025-03-25 19:12:28 +0900

    fisrt commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Leetcode 문제풀이용 프로젝트
+
+### 실행
+
+```
+$ npm start {문제번호}
+$ npm start 3994
+```
---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.js
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  prettier,
+];
