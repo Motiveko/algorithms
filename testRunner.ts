28abc78dd6a3d7fbacfcc9b922ce63c7ca67a1c2
motiveko <rhehdrla@naver.com>
2025-03-27 11:27:49 +0900

    runner 개선

--- testRunner.ts
+++ testRunner.ts
@@ -4,7 +4,10 @@ export const runner = <T extends (...args) => any>(
 ) => {
   testData.forEach((data, i) => {
     const result = func(...data.args);
-    const success = result === data.expected;
+    const success =
+      typeof result === "object" && result !== null
+        ? JSON.stringify(result) === JSON.stringify(data.expected)
+        : result === data.expected;
     const icon = success ? "✅" : "❌";
     console.log(
       `${icon} 테스트 ${i} - expected: ${data.expected}\treal: ${result}`
